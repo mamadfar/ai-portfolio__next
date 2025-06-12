3e7f2a6e0738a7eafbb15cc377696796636a823f
mamadfar <m.sabah.farhadi@gmail.com>
2025-06-12 14:48:24 +0200

    #feat: add download btn for the resume

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,23 +1,35 @@
-import Link from "next/link"
-import ThemeToggle from "./ThemeToggle"
-import AIChatButton from "../ai/AIChatButton"
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+import AIChatButton from "../ai/AIChatButton";
+import { DownloadCloud } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className='sticky top-0 bg-background'>
-        <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
-          <nav className="space-x-4 font-medium">
-            <Link href="/">home</Link>
-            <Link href="/about">about me</Link>
-            <Link href="/social">social media</Link>
-          </nav>
-          <div className="flex items-center gap-4">
-            <AIChatButton/>
-            <ThemeToggle/>
-          </div>
+    <header className="bg-background sticky top-0">
+      <div className="mx-auto flex max-w-3xl flex-wrap justify-between gap-3 px-3 py-4">
+        <nav className="space-x-4 font-medium">
+          <Link href="/">home</Link>
+          <Link href="/about">about me</Link>
+          <Link href="/social">social media</Link>
+        </nav>
+        <div className="flex items-center gap-4">
+          <AIChatButton />
+          <ThemeToggle />
+          <Link
+            href="/Mohammad_Farhadi_Frontend_Resume.pdf"
+            download
+            className="inline-flex items-center gap-1 hover:underline"
+            target="_self"
+            rel="noopener noreferrer"
+            aria-label="Download Resume"
+          >
+            <DownloadCloud className="h-5 w-5" />
+            resume
+          </Link>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
